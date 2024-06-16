8fb5f959857375753fc65047fcdc09d559642586
LeLeT-Prog6 <wbnominat@bk.ru>
2024-06-16 18:07:05 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+the_number_of_completed_tasks = "12"
+the_number_of_hours_spend = "1.5"
+cours_name  = "Pythone"
+print (12 // 1.5)
+print (cours_name , the_number_of_completed_tasks, the_number_of_hours_spend , "среднее время выполнения 0.125 часа")
